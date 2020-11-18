b606601c704d323e920260e8d7a24a5a623e3b45
Robin Bono <robin@omnick.se>
2020-11-18 16:00:51 +0100

    PlayerState init

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public static PlayerState instance;
+
+    public enum InteractState
+    {
+        select,
+        tool,
+        placement
+    }
+
+    public InteractState currentInteractState;
+    
+    void Start()
+    {
+        if( instance == null ) {
+
+            instance = this;
+        } else {
+
+            Destroy( this );
+        }
+        
+        currentInteractState = InteractState.select;
+    }
+}
