2d37c2c6b7abe8d079fa04681d856cc00810969b
Robin Bono <robin@omnick.se>
2020-11-18 15:59:22 +0100

    MouseOver script
    First version of MouseOver script used on interactable objects

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/MouseOver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOver : MonoBehaviour
+{
+    private SpriteRenderer renderer;
+    
+    private Color mouseOverColor;
+    private Color standardColor;
+
+    private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+
+        mouseOverColor = Color.red;
+        standardColor = renderer.material.color;
+
+        if (!gameObject.CompareTag("Interactable"))
+        {
+            gameObject.tag = "Interactable";
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        if (PlayerState.instance.currentInteractState == PlayerState.InteractState.select)
+        {
+            renderer.material.color = mouseOverColor;
+            PlayerInteract.instance.mouseOverItem = this.gameObject;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (PlayerState.instance.currentInteractState == PlayerState.InteractState.select)
+        {
+            renderer.material.color = standardColor;
+            PlayerInteract.instance.mouseOverItem = null;
+        }
+    }
+}
