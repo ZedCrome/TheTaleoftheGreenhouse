bc763b4d1c5966c25f23700f2befca30eebcef96
Robin Bono <robin@omnick.se>
2020-11-18 16:01:11 +0100

    Create PlayerInteract.cs

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public static PlayerInteract instance;
+    
+    public GameObject inventoryItem;
+    public GameObject mouseOverItem;
+    
+    private RaycastHit2D hit;
+    
+    void Start()
+    {
+        if( instance == null ) {
+
+            instance = this;
+        } else {
+
+            Destroy( this );
+        }
+    }
+    void Update()
+    {
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (mouseOverItem != null)
+            {
+                inventoryItem = mouseOverItem;
+                inventoryItem.SetActive(false);
+                mouseOverItem = null;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+        
+        if(hit.collider != null && hit.transform.CompareTag("Interactable"))
+        {
+            Debug.Log(hit.collider.name);
+
+            hit.transform.GetComponent<MouseOver>();
+        }
+    }
+}
