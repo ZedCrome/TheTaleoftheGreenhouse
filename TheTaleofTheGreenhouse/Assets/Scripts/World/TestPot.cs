cb81f86ff237d87ebb2f40ec20a23d7885e20774
Wiive <wirrechan@hotmail.com>
2020-11-18 15:53:51 +0100

    Created a test Table & Pot

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/World/TestPot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPot : MonoBehaviour
+{
+    //ItemSpot should be based on the object the mouse interacts with
+    public TestTableItemSpot itemSpot;
+
+    Vector3 position;
+
+    private void Start()
+    {
+        position = transform.position;
+    }
+
+    void OnMouseDown()
+    {
+        itemSpot.PlacedItem(TestTableItemSpot.GameItems.Pot);
+        Debug.Log("Trying to move object");
+        gameObject.transform.parent = itemSpot.transform;
+        gameObject.transform.position = itemSpot.transform.position;
+    }
+}
---
+++ TheTaleofTheGreenhouse/Assets/Scripts/World/TestTable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTable : MonoBehaviour
+{
+    public GameObject itemSpot1;
+    public GameObject itemSpot2;
+}
