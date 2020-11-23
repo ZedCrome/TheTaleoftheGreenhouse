effa5ede4060bc7d1cc1f575df49e642f6ac1b1b
Robin Bono <robin@omnick.se>
2020-11-23 16:19:03 +0100

    Beginning of save code

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Systems/DataManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public enum EntityType
+    {
+        Table,
+        Pot   
+    }
+    
+    [SerializeField]
+    private List<Entity> entity = new List<Entity>();
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private class Entity
+    {
+        public EntityType entityType;
+
+        public int id;
+        public GameObject gameObject;
+    }
+
+    public int AddEntity(GameObject newObject, EntityType type)
+    {
+        int freeIndex = CheckForFreeIndexSlot();
+
+        if (freeIndex > -1)
+        {
+            //entity[freeIndex].gameObject = 
+        }
+        else
+        {
+            
+        }
+        
+        //entity[newId].id = newId;
+        //entity[newId].gameObject = newObject;
+
+        //return newId;
+
+        return 1;
+    }
+    
+    private int CheckForFreeIndexSlot()
+    {
+        for (int i = 0; i < entity.Count; i++)
+        {
+            if (entity[i].gameObject == null)
+            {
+                return i;
+            }       
+        }
+
+        return -1;
+    }
+}
---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Systems/GridManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public GridLayout gridLayout;
+    public Vector3Int cellPosition;
+    void Start()
+    {
+        //gridLayout = GetComponent<GridLayout>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+
+        cellPosition = gridLayout.WorldToCell(Camera.main.ScreenToWorldPoint(mousePosition));
+    }
+}
