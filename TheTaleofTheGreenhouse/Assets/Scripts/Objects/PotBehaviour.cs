e092adaae7b100ab764c703c16d87710e71d03a8
Robin Bono <robin@omnick.se>
2020-11-25 16:16:13 +0100

    Update PotBehaviour.cs

--- TheTaleofTheGreenhouse/Assets/Scripts/Objects/PotBehaviour.cs
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/PotBehaviour.cs
@@ -2,7 +2,17 @@
 
 public class PotBehaviour : MonoBehaviour
 {
+    private bool isWatered = false;
     
+    public void FillWater()
+    {
+        isWatered = true;
+    }
+
+    public bool GetWatered()
+    {
+        return isWatered;
+    }
 }
 
  
