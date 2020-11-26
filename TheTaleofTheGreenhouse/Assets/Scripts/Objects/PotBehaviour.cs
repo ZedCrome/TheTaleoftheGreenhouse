ee87efaf10f7d59c74d25eab956fdcb1476f3300
Robin Bono <robin@omnick.se>
2020-11-26 16:10:04 +0100

    Pot sprite change from dry/wet

--- TheTaleofTheGreenhouse/Assets/Scripts/Objects/PotBehaviour.cs
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/PotBehaviour.cs
@@ -1,13 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PotBehaviour : MonoBehaviour
 {
     [SerializeField]
     private bool isWatered = false;
-    
+
+    private SpriteRenderer renderer;
+
+    public Sprite potDry;
+    public Sprite potWet;
+
+    private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+
+        if (isWatered)
+        {
+            renderer.sprite = potWet;
+        }
+        else
+        {
+            renderer.sprite = potDry;
+        }
+    }
+
     public void FillWater()
     {
         isWatered = true;
+        renderer.sprite = potWet;
         Debug.Log("Watered");
     }
 
@@ -19,6 +40,7 @@ public class PotBehaviour : MonoBehaviour
     public void EmptyWater()
     {
         isWatered = false;
+        renderer.sprite = potDry;
     }
 }
 
