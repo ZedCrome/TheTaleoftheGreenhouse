32c69fce7a138193f72e46dd9de0d6c07abb25f7
Wiive <wirrechan@hotmail.com>
2020-11-26 16:09:46 +0100

    Created First Iteration on Plants & Updated Main Scene

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/PlantStates.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlantStates : MonoBehaviour
+{
+    public Sprite deadSprite;
+    public Sprite fullGrownSprite;
+    public Sprite adultSprite;
+    public Sprite youngSprite;
+    public Sprite sproutSprite;
+    private Sprite currentSprite;
+
+    SpriteRenderer spriteRenderer;
+
+    public enum PlantState { Sprout, Young, Adult, FullGrown, Dead};
+    public PlantState currenState = 0;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+       
+    }
+
+    private void Update()
+    {
+        spriteRenderer.sprite = currentSprite;
+        switch (currenState)
+        {
+            case PlantState.Sprout:
+                {
+                    currentSprite = sproutSprite;
+                    break;
+                }
+            case PlantState.Young:
+                {
+                    currentSprite = youngSprite;
+                    break;
+                }
+            case PlantState.Adult:
+                {
+                    currentSprite = adultSprite;
+                    break;
+                }
+            case PlantState.FullGrown:
+                {
+                    //Can give Cuttings OR Mana
+                    currentSprite = fullGrownSprite;
+                    break;
+                }
+            case PlantState.Dead:
+                {
+                    currentSprite = deadSprite;
+                    break;
+                }
+        }
+    }
+
+    //take information of the night state
+    //update renderer
+}
