1de66460cf5a519a2e4df7efb99d4cbdf24a16af
ZedCrome <robin.lindevy@live.com>
2020-11-27 11:44:41 +0100

    Horrible but working "night"

--- TheTaleofTheGreenhouse/Assets/Scripts/Objects/BedBehavior.cs
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/BedBehavior.cs
@@ -1,28 +1,13 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BedBehavior : MonoBehaviour
 {
-    private InteractableEffect interactEffect;
-
-    private void Start()
-    {
-        interactEffect = GetComponent<InteractableEffect>();
-    }
-
     private void OnMouseOver()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            
+
             DayNightCycle.instance.Sleep();
         }
-    }
-
-    private void OnMouseExit()
-    {
-        
     }
 }
--- TheTaleoftheGreenhouse/Assets/Scripts/Systems/DayNightCycle.cs
+++ TheTaleoftheGreenhouse/Assets/Scripts/Systems/DayNightCycle.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 using Light2D = UnityEngine.Experimental.Rendering.Universal.Light2D;
 
+[DefaultExecutionOrder(-10)]
 public class DayNightCycle : MonoBehaviour
 {
+    
     public static DayNightCycle instance;
     public Light2D light;
     public float realSecondsPerIngameDay;
@@ -16,7 +19,6 @@ public class DayNightCycle : MonoBehaviour
 
     public string hourString;
     public string minutesString;
-    
 
     [SerializeField] Color dayColor;
     [SerializeField] Color eveningColor;
@@ -27,12 +29,17 @@ public class DayNightCycle : MonoBehaviour
     [SerializeField] float transitionTime = 2;
 
     private bool firstMorning = true;
+    private bool allowedToSleep = false;
+    
+    private float timer = 0;
 
     [SerializeField] Transform hourHandTransform;
     [SerializeField] Transform minuteHandTransform;
-    
+    [SerializeField] TMP_Text sleepText;
 
-    
+    [SerializeField] GameObject player;
+
+
     private void Awake()
     {
         if (instance == null)
@@ -53,11 +60,13 @@ public class DayNightCycle : MonoBehaviour
     
     void Update()
     {
+        Debugger.instance.Log("Allowed To Sleep: ", allowedToSleep);
+        
         day += Time.deltaTime / realSecondsPerIngameDay;
         
         float dayNormalized = day % 1f;
         
-       hourString = Mathf.Floor(dayNormalized * hoursPerDay).ToString("00");
+        hourString = Mathf.Floor(dayNormalized * hoursPerDay).ToString("00");
 
         minutesString = Mathf.Floor(((dayNormalized * hoursPerDay) % 1f) * minutesPerHour).ToString("00");
 
@@ -65,25 +74,31 @@ public class DayNightCycle : MonoBehaviour
         hourHandTransform.eulerAngles = new Vector3(0, 0, -dayNormalized * rotationDegreesPerDay);
         minuteHandTransform.eulerAngles = new Vector3(0, 0, -dayNormalized * rotationDegreesPerDay * hoursPerDay);
         
+
         lightChange();
     }
 
 
     void lightChange()
     {
-        float timer = 0;
+        
         if (float.Parse(hourString) > 18 && float.Parse(hourString) < 24f)
         {
             
             timer += Time.deltaTime;
+            allowedToSleep = true;
             light.intensity = Mathf.Lerp(dayIntensity, eveningIntensity, transitionTime * timer);
             light.color = Color.Lerp(dayColor, eveningColor, transitionTime * timer);
             firstMorning = false;
         } 
-        else if (float.Parse(hourString) > 5 && float.Parse(hourString) < 11f && !firstMorning)
+        
+        else if (float.Parse(hourString) > 6 && float.Parse(hourString) < 11f && !firstMorning)
         {
-            
+            sleepText.text = "";
+            player.GetComponent<PlayerMovement>().enabled = true;
+            player.GetComponent<PlayerRenderer>().enabled = true;
             timer += Time.deltaTime;
+            allowedToSleep = false;
             light.intensity = Mathf.Lerp(eveningIntensity, dayIntensity, transitionTime * timer);
             light.color = Color.Lerp(eveningColor, dayColor, transitionTime * timer);
         }
@@ -97,24 +112,14 @@ public class DayNightCycle : MonoBehaviour
 
     public void Sleep()
     {
-        float timer = 0;
-        if (float.Parse(hourString) > 22 && float.Parse(hourString) < 2)
-        {
-            timer += Time.deltaTime;
-            light.intensity = Mathf.Lerp(eveningIntensity, nightIntensity, transitionTime * 3 * timer);
-            light.color = Color.Lerp(eveningColor, nightColor, transitionTime * 3 * timer);
-        }
-        else if (float.Parse(hourString) > 6 && float.Parse(hourString) < 11)
-        {
-            timer += Time.deltaTime;
-            light.intensity = Mathf.Lerp(nightIntensity, dayIntensity, transitionTime * 3 * timer);
-            light.color = Color.Lerp(nightColor, dayColor, transitionTime * 3 * timer);
-        }
-        else
-        {
-            timer = 0f;
-        }
         
-        onSleep?.Invoke();
+        if (allowedToSleep)
+        {
+            sleepText.text = "Sleeping";
+            player.GetComponent<PlayerMovement>().enabled = false;
+            player.GetComponent<PlayerRenderer>().enabled = false;
+            onSleep?.Invoke();
+        }
+            
     }
 }
