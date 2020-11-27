575b69ad6965bbf698caa3c9442f89b97dc185c0
Robin Bono <robin@omnick.se>
2020-11-27 10:56:56 +0100

    Sleep function bed

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/BedBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedBehavior : MonoBehaviour
+{
+    private InteractableEffect interactEffect;
+
+    private void Start()
+    {
+        interactEffect = GetComponent<InteractableEffect>();
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            
+            DayNightCycle.instance.Sleep();
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        
+    }
+}
