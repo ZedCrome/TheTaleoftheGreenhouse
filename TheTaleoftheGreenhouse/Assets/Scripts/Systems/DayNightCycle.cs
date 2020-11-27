5c0ff9a9addd5bfd313e73b7942213b914d7dd23
ZedCrome <robin.lindevy@live.com>
2020-11-27 09:50:05 +0100

    First night iteration (probably very buggy and may not work)

--- TheTaleoftheGreenhouse/Assets/Scripts/Systems/DayNightCycle.cs
+++ TheTaleoftheGreenhouse/Assets/Scripts/Systems/DayNightCycle.cs
@@ -16,13 +16,14 @@ public class DayNightCycle : MonoBehaviour
 
     public string hourString;
     public string minutesString;
-
-    private float timer;
+    
 
     [SerializeField] Color dayColor;
+    [SerializeField] Color eveningColor;
     [SerializeField] Color nightColor;
     [SerializeField] float dayIntensity = 1f;
-    [SerializeField] float nightIntensity = 0.75f;
+    [SerializeField] float eveningIntensity = 0.75f;
+    [SerializeField] float nightIntensity = 0f;
     [SerializeField] float transitionTime = 2;
 
     private bool firstMorning = true;
@@ -70,20 +71,21 @@ public class DayNightCycle : MonoBehaviour
 
     void lightChange()
     {
+        float timer = 0;
         if (float.Parse(hourString) > 18 && float.Parse(hourString) < 24f)
         {
             
             timer += Time.deltaTime;
-            light.intensity = Mathf.Lerp(1f, nightIntensity, transitionTime * timer);
-            light.color = Color.Lerp(dayColor, nightColor, transitionTime * timer);
+            light.intensity = Mathf.Lerp(dayIntensity, eveningIntensity, transitionTime * timer);
+            light.color = Color.Lerp(dayColor, eveningColor, transitionTime * timer);
             firstMorning = false;
         } 
         else if (float.Parse(hourString) > 5 && float.Parse(hourString) < 11f && !firstMorning)
         {
             
             timer += Time.deltaTime;
-            light.intensity = Mathf.Lerp(nightIntensity, dayIntensity, transitionTime * timer);
-            light.color = Color.Lerp(nightColor, dayColor, transitionTime * timer);
+            light.intensity = Mathf.Lerp(eveningIntensity, dayIntensity, transitionTime * timer);
+            light.color = Color.Lerp(eveningColor, dayColor, transitionTime * timer);
         }
         else
         {
@@ -95,6 +97,24 @@ public class DayNightCycle : MonoBehaviour
 
     public void Sleep()
     {
+        float timer = 0;
+        if (float.Parse(hourString) > 22 && float.Parse(hourString) < 2)
+        {
+            timer += Time.deltaTime;
+            light.intensity = Mathf.Lerp(eveningIntensity, nightIntensity, transitionTime * 3 * timer);
+            light.color = Color.Lerp(eveningColor, nightColor, transitionTime * 3 * timer);
+        }
+        else if (float.Parse(hourString) > 6 && float.Parse(hourString) < 11)
+        {
+            timer += Time.deltaTime;
+            light.intensity = Mathf.Lerp(nightIntensity, dayIntensity, transitionTime * 3 * timer);
+            light.color = Color.Lerp(nightColor, dayColor, transitionTime * 3 * timer);
+        }
+        else
+        {
+            timer = 0f;
+        }
+        
         onSleep?.Invoke();
     }
 }
