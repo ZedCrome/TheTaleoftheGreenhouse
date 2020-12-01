eb2a4dd00d4e30f97e2ae0585d906288470076de
Wiive <wirrechan@hotmail.com>
2020-12-01 15:20:14 +0100

    Working on ManaSystem with ManaCube and ManaCatcher

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/ManaCatcherBehavior.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaCatcherBehavior : MonoBehaviour
+{
+    public AudioClip storeManaSound;
+    public AudioClip alreadyFull;
+
+    private AudioSource audioSource;
+    private ManaCubeBehavior manaCubeBehavior;
+
+    public int currentMana;
+
+    private bool rightMouseButtonLock = false;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (PlayerState.instance.currentHandState == PlayerState.HandState.ManaCatcher)
+        {
+            if (Input.GetMouseButtonDown(1) && rightMouseButtonLock == false)
+            {
+                if (AllowedToDoAction())
+                {
+                    //Make a new cool Action to be able to move around mana
+                    if (PlayerInteract.instance.interactObject.transform.GetComponent<ManaCubeBehavior>().GetMana() == manaCubeBehavior.maxMana)
+                    {
+                        Debug.Log("Dont add mana cuz the cube is full");
+                        audioSource.PlayOneShot(alreadyFull);
+                    }
+                    else
+                    {
+                        Debug.Log("Trying ti add mana to mana cube");
+                        audioSource.PlayOneShot(storeManaSound);
+                        PlayerInteract.instance.interactObject.transform.GetComponent<ManaCubeBehavior>().AddMana(currentMana);
+                    }
+
+                }
+
+                rightMouseButtonLock = true;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            rightMouseButtonLock = false;
+        }
+    }
+
+
+    //Only works on ManaStorage right now.
+    private bool AllowedToDoAction()
+    {
+        if (PlayerInteract.instance.allowedTointeract)
+        {
+            if (PlayerInteract.instance.interactObject != null)
+            {
+                if (PlayerInteract.instance.interactObject.CompareTag("ManaStorage"))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
--- TheTaleofTheGreenhouse/Assets/Scripts/Objects/ManaCubeBehavior.cs
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/ManaCubeBehavior.cs
@@ -2,21 +2,63 @@
 
 public class ManaCubeBehavior : MonoBehaviour
 {
-    public AudioClip storeManaSound;
-    public AudioClip alreadyFull;
+    SpriteRenderer spriteRenderer;
+    
+    public Sprite emptyCube;
+    public Sprite firstStageCube;
+    public Sprite seccondStageCube;
+    public Sprite fullStageCube;
 
-    public int incomingMana;
-    private int storedMana = 0;
-    private int maxMana = 5;
+    private int emptyCubeValue = 0;
+    private int firstStageCubeValue = 5;
+    private int seccondStageCubeValue = 10;
+    private int fullStageCubeValue = 15;
 
-    //Make sure user have the mana catcher on it.
-    //Check how much mana the mana catcher have
+    public int storedMana = 0;
+    public int maxMana = 15;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {    
+        if (storedMana > emptyCubeValue && storedMana < seccondStageCubeValue)
+        {
+            spriteRenderer.sprite = firstStageCube;
+        }
+
+        else if (storedMana > firstStageCubeValue && storedMana < fullStageCubeValue)
+        {
+            spriteRenderer.sprite = seccondStageCube;
+        }
+
+        else if (storedMana >= fullStageCubeValue)
+        {
+            spriteRenderer.sprite = fullStageCube;
+            storedMana = maxMana;
+        }
+
+        else
+        {
+            spriteRenderer.sprite = emptyCube;
+        }
+    }
 
     public void AddMana(int mana)
     {
-        incomingMana = mana;
-        storedMana += incomingMana;
+        storedMana += mana;
     }
 
+    public int GetMana()
+    {       
+        return storedMana;
+    }
 
+    public void EmptyMana()
+    {
+        storedMana = 0;
+        spriteRenderer.sprite = emptyCube;
+    }
 }
