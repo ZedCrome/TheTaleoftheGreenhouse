7110ec87f35e7b849b2fc7c45f99e266d5ccf9d5
Robin Bono <robin@omnick.se>
2020-12-01 11:08:57 +0100

    Update on shears functionality

--- TheTaleofTheGreenhouse/Assets/Scripts/Objects/ShearsBehaviour.cs
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/ShearsBehaviour.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class ShearsBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip cutSound;
+    public AudioClip alreadyCut;
+
+    private AudioSource audioSource;
+    
+    private bool rightMouseButtonLock = false;
+
+    private void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (PlayerState.instance.currentHandState == PlayerState.HandState.Shears)
+        {
+            if (Input.GetMouseButtonDown(1) && rightMouseButtonLock == false)
+            {
+                if (AllowedToDoAction())
+                {
+                    if (PlayerInteract.instance.interactObject.GetComponent<ObjectSlot>().objectInSlot.GetComponent<PlantStates>().CutPlant() == true)
+                    {
+                        PlayerInventory.instance.AddSapling();
+                        audioSource.PlayOneShot(cutSound);
+                    }
+                    else
+                    {
+                        audioSource.PlayOneShot(alreadyCut);
+                    }
+                }
+
+                rightMouseButtonLock = true;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            rightMouseButtonLock = false;
+        }
+    }
+
+    private bool AllowedToDoAction()
+    {
+        if (PlayerInteract.instance.allowedTointeract)
+        {
+            if (PlayerInventory.instance.CanCarryMoreSaplings()) 
+            {
+                if (PlayerInteract.instance.interactObject != null)
+                {
+                    if (PlayerInteract.instance.interactObject.CompareTag("PotSlot"))
+                    {
+                        if (PlayerInteract.instance.interactObject.GetComponent<ObjectSlot>().objectInSlot != null)
+                        {
+                            if (PlayerInteract.instance.interactObject.GetComponent<ObjectSlot>().objectInSlot.CompareTag("PlantNormal") ||
+                                PlayerInteract.instance.interactObject.GetComponent<ObjectSlot>().objectInSlot.CompareTag("PlantMana"))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
