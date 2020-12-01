874a8f4954a3f90127bc1b938c39cbf356508cee
Robin Bono <robin@omnick.se>
2020-12-01 15:19:25 +0100

    weather test

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Systems/WeatherManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private Animator animator;
+
+    public AudioClip audioClip;
+    public bool fire;
+    
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (fire)
+        {
+            animator.SetTrigger("PlayLightning");
+            audioSource.PlayOneShot(audioClip);
+            fire = false;
+        }   
+    }
+}
