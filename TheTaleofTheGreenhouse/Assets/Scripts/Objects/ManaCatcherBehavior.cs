9eaf6eb3a8082ace88e7afabf286c33ffee95ccc
Wiive <wirrechan@hotmail.com>
2020-12-02 10:17:54 +0100

    Can Stack ManaCubes

--- TheTaleofTheGreenhouse/Assets/Scripts/Objects/ManaCatcherBehavior.cs
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/ManaCatcherBehavior.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class ManaCatcherBehavior : MonoBehaviour
 {
     public AudioClip storeManaSound;
-    public AudioClip alreadyFull;
+    public AudioClip noManaAction;
 
     private AudioSource audioSource;
     private ManaCubeBehavior manaCubeBehavior;
 
+
+    private SpriteRenderer spriteRenderer;
     public Sprite emptyCatcher;
     public Sprite firstStageCatcher;
     public Sprite fullStageCatcher;
 
+    private int emptyValue = 0;
+    private int fullValue = 10;
+
     public int currentMana;
+    private int giveMana = 5;
 
     private bool rightMouseButtonLock = false;
 
@@ -22,10 +28,27 @@ public class ManaCatcherBehavior : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();
         manaCubeBehavior = new ManaCubeBehavior();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
+        if (currentMana > emptyValue && currentMana < fullValue)
+        {
+            spriteRenderer.sprite = firstStageCatcher;
+        }
+
+        else if (currentMana >= fullValue)
+        {
+            spriteRenderer.sprite = fullStageCatcher;
+        }
+
+        else
+        {
+            spriteRenderer.sprite = emptyCatcher;
+        }
+
+
         if (PlayerState.instance.currentHandState == PlayerState.HandState.ManaCatcher)
         {
             if (Input.GetMouseButtonDown(1) && rightMouseButtonLock == false)
@@ -34,12 +57,17 @@ public class ManaCatcherBehavior : MonoBehaviour
                 {
                     if (PlayerInteract.instance.interactObject.GetComponent<ObjectSlot>().objectInSlot.GetComponent<ManaCubeBehavior>().GetMana() == manaCubeBehavior.maxMana)
                     {
-                        audioSource.PlayOneShot(alreadyFull);
+                        audioSource.PlayOneShot(noManaAction);
+                    }
+                    else if (currentMana > 0)
+                    {
+                        audioSource.PlayOneShot(storeManaSound);
+                        PlayerInteract.instance.interactObject.GetComponent<ObjectSlot>().objectInSlot.GetComponent<ManaCubeBehavior>().AddMana(giveMana);
+                        LoseMana();
                     }
                     else
                     {
-                        audioSource.PlayOneShot(storeManaSound);
-                        PlayerInteract.instance.interactObject.GetComponent<ObjectSlot>().objectInSlot.GetComponent<ManaCubeBehavior>().AddMana(currentMana);
+                        audioSource.PlayOneShot(noManaAction);
                     }
                 }
                 rightMouseButtonLock = true;
@@ -51,7 +79,6 @@ public class ManaCatcherBehavior : MonoBehaviour
             rightMouseButtonLock = false;
         }
     }
-
 
     //Only works on ManaStorage right now.
     private bool AllowedToDoAction()
@@ -69,7 +96,20 @@ public class ManaCatcherBehavior : MonoBehaviour
                 }
             }
         }
-
         return false;
     }
+
+    private void LoseMana()
+    {
+        if (currentMana >= 5)
+        {
+            currentMana -= 5;
+        }
+        else
+        {
+            audioSource.PlayOneShot(noManaAction);
+        }
+    }
+
+
 }
