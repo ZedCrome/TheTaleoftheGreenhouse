92396baa9b02ef4d358aaea38d27b5d3fa8705ae
ZedCrome <robin.lindevy@live.com>
2020-12-02 13:19:37 +0100

    ShopUI stuff and improved Day/night-cycle

---
+++ TheTaleoftheGreenhouse/Assets/LeanTweenAnimateBuyMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LeanTweenAnimateBuyMenu : MonoBehaviour
+{
+    public void exit()
+    {
+        LeanTween.scale(gameObject, new Vector3(0, 0, 0), 0.5f).setOnComplete(closeShop);
+    }
+
+    void closeShop()
+    {
+        gameObject.SetActive(false);
+    }
+}
