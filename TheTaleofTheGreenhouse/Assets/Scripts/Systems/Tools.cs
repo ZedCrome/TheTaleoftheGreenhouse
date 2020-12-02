913ebe48f53ff7ba501ea10bada4df360825b4fa
Robin Bono <robin@omnick.se>
2020-12-02 16:18:02 +0100

    Update Tools.cs

--- TheTaleofTheGreenhouse/Assets/Scripts/Systems/Tools.cs
+++ TheTaleofTheGreenhouse/Assets/Scripts/Systems/Tools.cs
@@ -6,6 +6,12 @@ public class Tools : MonoBehaviour
 {
     public static AudioClip GetRandomSound(AudioClip[] audioClip)
     {
+        if (audioClip.Length == 0)
+        {
+            Debug.LogError("AudioClip does not contain any audio files");
+            return null;
+        }
+        
         int random = Random.Range(0, audioClip.Length);
 
         return audioClip[random];
