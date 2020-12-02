d978d9417e5c334aed556717e415df64ac824d9f
Robin Bono <robin@omnick.se>
2020-12-02 14:56:15 +0100

    Added tools script
    can be used to put reduntant functions to be shared between scripts

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Systems/Tools.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tools : MonoBehaviour
+{
+    public static AudioClip GetRandomSound(AudioClip[] audioClip)
+    {
+        int random = Random.Range(0, audioClip.Length);
+
+        return audioClip[random];
+    }
+}
