1015ff6e218b78ed9e700979d8277c6e1eaf4ec7
Wiive <wirrechan@hotmail.com>
2020-12-04 10:40:26 +0100

    Updated Delivery Prefab & GameManger Prefab

--- TheTaleofTheGreenhouse/Assets/Scripts/Objects/DeliveryManager.cs
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/DeliveryManager.cs
@@ -3,17 +3,11 @@ using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
-    public GameObject potPrefab;
-    public GameObject planManatPrefab;
-    public GameObject plantNormalPrefab;
-    public GameObject manaCubePrefab;
-    public GameObject tablePrefab;
-
     public bool spawnPot;
     public bool spawnPlantMana;
     public bool spawnPlantNormal;
-    public bool spawnPlantCube;
-    public bool spawnPlantTable;
+    public bool spawnManaCube;
+    //public bool spawnPlantTable;
 
     private List<ObjectSlot> objectSlot = new List<ObjectSlot>();
     
@@ -34,8 +28,8 @@ public class DeliveryManager : MonoBehaviour
             {
                 if (slot.objectInSlot == null)
                 {
-                    GameObject clone = Instantiate(potPrefab);
-                    
+                    GameObject clone = PrefabManager.instance.CreateNewObjectInstance("Pot");
+
                     slot.FillSlot(clone);
 
                     spawnPot = false;
@@ -44,6 +38,65 @@ public class DeliveryManager : MonoBehaviour
             }
 
             spawnPot = false;
+        }
+
+
+        if (spawnPlantMana)
+        {
+            foreach (ObjectSlot slot in objectSlot)
+            {
+                if (slot.objectInSlot == null)
+                {
+                    GameObject clone = PrefabManager.instance.CreateNewObjectInstance("PlantMana");
+
+                    clone.transform.localScale = new Vector3(1, 1, 1);
+                    slot.FillSlot(clone);
+
+                    spawnPlantMana = false;
+                    break;
+                }
+            }
+
+            spawnPlantNormal = false;
+        }
+
+
+        if (spawnPlantNormal)
+        {
+            foreach (ObjectSlot slot in objectSlot)
+            {
+                if (slot.objectInSlot == null)
+                {
+                    GameObject clone = PrefabManager.instance.CreateNewObjectInstance("PlantNormal");
+
+                    clone.transform.localScale = new Vector3(1, 1, 1);
+                    slot.FillSlot(clone);
+
+                    spawnPlantNormal = false;
+                    break;
+                }
+            }
+
+            spawnPlantNormal = false;
+        }
+
+
+        if (spawnManaCube)
+        {
+            foreach (ObjectSlot slot in objectSlot)
+            {
+                if (slot.objectInSlot == null)
+                {
+                    GameObject clone = PrefabManager.instance.CreateNewObjectInstance("ManaStorage");
+
+                    slot.FillSlot(clone);
+
+                    spawnPlantNormal = false;
+                    break;
+                }
+            }
+
+            spawnManaCube = false;
         }
     }
 }
