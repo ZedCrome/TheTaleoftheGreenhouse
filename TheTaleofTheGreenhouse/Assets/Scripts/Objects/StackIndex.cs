2f995fcd4b11f133a242f189e81d3c252f639c16
Robin Bono <robin@omnick.se>
2020-12-08 13:30:15 +0100

    added stack index to items and moved some more items into correct folder

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/StackIndex.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackIndex : MonoBehaviour
+{
+    private int indexNumber;
+
+    public int IndexNumber
+    {
+        get { return indexNumber; }
+        set { indexNumber = value; }
+    }
+}
