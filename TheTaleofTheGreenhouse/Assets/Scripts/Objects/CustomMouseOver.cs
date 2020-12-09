2bdc657e7f3a7fac8fa78c78ce5cb4c989c0e2c5
Robin Bono <robin@omnick.se>
2020-12-09 15:39:22 +0100

    Book outline

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/CustomMouseOver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomMouseOver : MonoBehaviour
+{
+    private InteractableEffect interactableEffect;
+
+    private void Start()
+    {
+        interactableEffect = GetComponent<InteractableEffect>();
+    }
+
+    private void OnMouseOver()
+    {
+        interactableEffect.Enable(true);
+    }
+
+    private void OnMouseExit()
+    {
+        interactableEffect.Enable(false);
+    }
+}
