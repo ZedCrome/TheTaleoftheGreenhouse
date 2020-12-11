3604077f64d2818d0ae0fed86f4c53f1e4b03c33
Robin Bono <robin@omnick.se>
2020-12-11 13:02:51 +0100

    Update LightFlicker.cs
    added range

--- TheTaleofTheGreenhouse/Assets/Scripts/Objects/LightFlicker.cs
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/LightFlicker.cs
@@ -7,39 +7,66 @@ public class LightFlicker : MonoBehaviour
 {
     private Light2D light;
 
-    public float minIntensity = 0.1f;
-    public float maxIntensity = 0.5f;
-    public float stepModifier = 1.5f;
+    public float minIntensity = 0.2f;
+    public float maxIntensity = 0.4f;
+    public float intensityStepModifier = 0.9f;
+    public float minRange = 0.8f;
+    public float maxRange = 1.0f;
+    public float rangeStepModifier = 0.8f;
+    
     private float currentIntensity;
-    private bool direction;
+    private bool intensityDirection;
+    private float currentRange;
+    private bool rangeDirection;
+    
     
     private void Start()
     {
         light = GetComponent<Light2D>();
         currentIntensity = Random.Range(minIntensity, maxIntensity);
+        currentRange = Random.Range(minRange, maxRange);
     }
 
     private void Update()
     {
         if (currentIntensity < minIntensity)
         {
-            direction = true;
+            intensityDirection = true;
         }
-
         if (currentIntensity > maxIntensity)
         {
-            direction = false;
+            intensityDirection = false;
         }
 
-        if (direction)
+        if (currentRange < minRange)
         {
-            currentIntensity += stepModifier * Time.deltaTime;
+            rangeDirection = true;
+        }
+
+        if (currentRange > maxRange)
+        {
+            rangeDirection = false;
+        }
+
+        if (intensityDirection)
+        {
+            currentIntensity += intensityStepModifier * Time.deltaTime;
         }
         else
         {
-            currentIntensity -= stepModifier * Time.deltaTime;
+            currentIntensity -= intensityStepModifier * Time.deltaTime;
+        }
+
+        if (rangeDirection)
+        {
+            currentRange += rangeStepModifier * Time.deltaTime;
+        }
+        else
+        {
+            currentRange -= rangeStepModifier * Time.deltaTime;
         }
         
         light.intensity = currentIntensity;
+        light.pointLightOuterRadius = currentRange;
     }
 }
