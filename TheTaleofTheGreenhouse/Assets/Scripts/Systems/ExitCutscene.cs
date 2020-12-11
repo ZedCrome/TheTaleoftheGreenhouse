586b76607d0bb52095d7c9f7e385ca491b92ebc9
Robin Bono <robin@omnick.se>
2020-12-11 11:49:45 +0100

    Cutscene functions

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Systems/ExitCutscene.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Timeline;
+
+public class ExitCutscene : MonoBehaviour
+{
+    public TimelineAsset timeLine;
+    public double currentTime;
+    public double timeLineLength;
+    
+    private void Start()
+    {
+        timeLineLength = timeLine.duration;
+    }
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime > timeLineLength)
+        {
+            GameManager.instance.ChangeGameState(GameManager.GameState.GameLoop);
+
+            Scene currentScene = SceneManager.GetActiveScene();
+            Debug.Log(currentScene.name);
+            if (currentScene.name == "IntroCinematic")
+            {
+                SceneManager.LoadScene("MainScene");
+            }
+            else
+            {
+                SceneManager.LoadScene("MainMenuScene");
+            }
+        }
+    }
+}
