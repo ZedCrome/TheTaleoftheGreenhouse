2681938e29069a1bb396ddbdf3bd2ede21b99123
Robin Bono <robin@omnick.se>
2020-12-11 12:16:28 +0100

    Light flicker

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/LightFlicker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using Random = UnityEngine.Random;
+
+public class LightFlicker : MonoBehaviour
+{
+    private Light2D light;
+
+    public float minIntensity = 0.1f;
+    public float maxIntensity = 0.5f;
+    public float stepModifier = 1.5f;
+    private float currentIntensity;
+    private bool direction;
+    
+    private void Start()
+    {
+        light = GetComponent<Light2D>();
+        currentIntensity = Random.Range(minIntensity, maxIntensity);
+    }
+
+    private void Update()
+    {
+        if (currentIntensity < minIntensity)
+        {
+            direction = true;
+        }
+
+        if (currentIntensity > maxIntensity)
+        {
+            direction = false;
+        }
+
+        if (direction)
+        {
+            currentIntensity += stepModifier * Time.deltaTime;
+        }
+        else
+        {
+            currentIntensity -= stepModifier * Time.deltaTime;
+        }
+        
+        light.intensity = currentIntensity;
+    }
+}
