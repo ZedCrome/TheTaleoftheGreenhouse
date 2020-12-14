34724ea0bccf1960547a50260b92c558f9128315
Robin Bono <robin@omnick.se>
2020-12-14 11:26:59 +0100

    TableManager

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Systems/TableManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableManager : MonoBehaviour
+{
+    public static TableManager instance;
+
+    public int unlockedTables;
+    
+    public GameObject[] table;
+
+    private void Awake()
+    {
+        if( instance == null ) {
+
+            instance = this;
+        } else {
+
+            Destroy( this );
+        }
+    }
+
+    private void OnEnable()
+    {
+        DayNightCycle.instance.onSleep += OnSleep;
+    }
+    
+    private void OnDisable()
+    {
+        DayNightCycle.instance.onSleep -= OnSleep;
+    }
+
+    void OnSleep()
+    {
+        for (int i = 0; i < table.Length; i++)
+        {
+            if (i <= unlockedTables)
+            {
+                table[i].SetActive(true);
+            }
+            else
+            {
+                table[i].SetActive(false);
+            }
+        }
+    }
+}
