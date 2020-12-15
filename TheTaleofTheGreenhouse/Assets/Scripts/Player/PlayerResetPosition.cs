ba7ba2d8d0147226cfe7de3bb3b53091e8fcb916
Robin Bono <robin@omnick.se>
2020-12-15 15:10:44 +0100

    Player reset position when sleeping

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Player/PlayerResetPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerResetPosition : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        DayNightCycle.instance.onSleep += OnSleep;
+    }
+    
+    private void OnDisable()
+    {
+        DayNightCycle.instance.onSleep -= OnSleep;
+    }
+    
+    void OnSleep()
+    {
+        transform.localPosition = new Vector3(-0.6579178f, 0.8477007f, 0.0f);
+    }
+}
