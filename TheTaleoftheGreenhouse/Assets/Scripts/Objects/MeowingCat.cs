4f58ee9b2dbc309684007830b476b32614fb0b7c
Robin Bono <robin@omnick.se>
2020-12-18 11:18:05 +0100

    Update MeowingCat.cs
    cat multiple mjau sounds

--- TheTaleoftheGreenhouse/Assets/Scripts/Objects/MeowingCat.cs
+++ TheTaleoftheGreenhouse/Assets/Scripts/Objects/MeowingCat.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class MeowingCat : MonoBehaviour
 {
+    public AudioSource catAudioSource;
 
-    public AudioSource catMeowing;
-
-
+    public AudioClip[] catMeows;
+       
     private void OnMouseOver()
     {
         if (Input.GetKey(KeyCode.Mouse1) && Input.GetKey(KeyCode.Mouse1))
         {
-            catMeowing.Play();
+            catAudioSource.PlayOneShot(Tools.GetRandomSound(catMeows));
         }
     }
 }
