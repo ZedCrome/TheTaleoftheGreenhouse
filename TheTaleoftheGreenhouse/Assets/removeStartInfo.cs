f8a4a638840c4efc71d21018a751aed811210005
ZedCrome <robin.lindevy@live.com>
2020-12-18 10:43:54 +0100

    Added info-panel in the beginning

---
+++ TheTaleoftheGreenhouse/Assets/removeStartInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class removeStartInfo : MonoBehaviour
+{
+    public RectTransform beginnerInfo;
+    public bool exitInfo;
+    private void Start()
+    {
+        StartCoroutine(removeInfo());
+    }
+
+    public IEnumerator removeInfo()
+    {
+        while (exitInfo == false)
+        {
+            LeanTween.scale(beginnerInfo, new Vector3(1.05f, 1.05f, 1.05f), 1).setEaseInOutSine();
+            yield return new WaitForSeconds(1);
+            LeanTween.scale(beginnerInfo, new Vector3(1f, 1f, 1f), 1).setEaseInOutSine();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+
+    public void ExitInfo()
+    {
+        LeanTween.moveX(beginnerInfo, Screen.width * 1.5f, 3f).setEaseLinear();
+    }
+
+}
