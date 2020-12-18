de6d85291baf0243c3d002704cbb2ffdf4dea97c
ZedCrome <robin.lindevy@live.com>
2020-12-18 11:13:09 +0100

    Switched place on some scripts and updated Main-scene

---
+++ TheTaleoftheGreenhouse/Assets/Scripts/Objects/MeowingCat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeowingCat : MonoBehaviour
+{
+
+    public AudioSource catMeowing;
+
+
+    private void OnMouseOver()
+    {
+        if (Input.GetKey(KeyCode.Mouse1) && Input.GetKey(KeyCode.Mouse1))
+        {
+            catMeowing.Play();
+        }
+    }
+}
