8b95472742b42343e62b755bc3183622d8598930
Robin Bono <robin@omnick.se>
2021-01-09 16:55:00 +0100

    Trowel tool test

---
+++ TheTaleofTheGreenhouse/Assets/Scripts/Objects/TrowelBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrowelBehaviour : MonoBehaviour
+{
+    // private bool rightMouseButtonLock = false;
+    //
+    //     private void Update()
+    // {
+    //     if (PlayerState.instance.currentHandState == PlayerState.HandState.Trowel)
+    //     {
+    //         if (Input.GetMouseButtonDown(1) && rightMouseButtonLock == false)
+    //         {
+    //             if (AllowedToDoAction())
+    //             {
+    //                 PlayerInteract.instance.PickUp(PlayerInteract.instance.interactObject.GetComponent<ObjectSlot>().GetThisObject());
+    //             }
+    //
+    //             rightMouseButtonLock = true;
+    //         }
+    //     }
+    //     else if (PlayerInventory.instance.trowelEquiped != null)
+    //     {
+    //         
+    //     }
+    //
+    //     if (Input.GetMouseButtonUp(1))
+    //     {
+    //         rightMouseButtonLock = false;
+    //     }
+    // }
+    //
+    // private bool AllowedToDoAction()
+    // {
+    //     if (PlayerInteract.instance.allowedTointeract)
+    //     {
+    //         if (PlayerInventory.instance.CanCarryMoreCuttings()) 
+    //         {
+    //             if (PlayerInteract.instance.interactObject != null)
+    //             {
+    //                 if (PlayerInteract.instance.interactObject.CompareTag("PotSlot"))
+    //                 {
+    //                     if (PlayerInteract.instance.interactObject.GetComponent<ObjectSlot>().objectInSlot != null)
+    //                     {
+    //                         if (PlayerInteract.instance.interactObject.GetComponent<ObjectSlot>().objectInSlot.CompareTag("PlantNormal") ||
+    //                             PlayerInteract.instance.interactObject.GetComponent<ObjectSlot>().objectInSlot.CompareTag("PlantMana"))
+    //                         {
+    //                             return true;
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //
+    //     return false;
+    // }
+}
